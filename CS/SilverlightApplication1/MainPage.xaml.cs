b93d4166ef85dc19e5d7b9db9ce795cfa2135915
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication1/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.XtraScheduler;
+
+namespace SilverlightApplication1 {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+
+            schedulerControl1.Start = new System.DateTime(2010, 7, 19, 0, 0, 0, 0);
+
+        }
+
+        // Commit changes to the database.
+        private void SchedulerStorage_AppointmentCollectionModified(object sender, EventArgs e) {
+            if (domainDataSource1.HasChanges) {
+                domainDataSource1.SubmitChanges();
+            }
+        }
+
+        private void DomainDataSource_LoadedData(object sender, LoadedDataEventArgs e) {
+            if (e.HasError) {
+                MessageBox.Show("Connection could not be established." + Environment.NewLine +
+                                e.Error.Message, "Connection Error", MessageBoxButton.OK);
+                e.MarkErrorAsHandled();
+            }
+        }
+
+        private void DomainDataSource_SubmittedChanges(object sender, SubmittedChangesEventArgs e) {
+            if (e.HasError) {
+                MessageBox.Show(e.Error.ToString(), "Submit Changes Error", MessageBoxButton.OK);
+                e.MarkErrorAsHandled();
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,15 @@
+# DXScheduler Getting Started - Lesson 5 - Provide Ribbon UI for a Scheduler
+
+
+<p>This example demonstrates how to provide end-users with basic functionality for switching and navigating views, and grouping scheduler's data via Ribbon UI.</p>
+
+
+<h3>Description</h3>
+
+<p>You can replicate the same project by performing steps from the following tutorial: <a href="http://documentation.devexpress.com/#Silverlight/CustomDocument5868"><u>DXScheduler - Provide Ribbon UI for a Scheduler</u></a>.</p>
+<p>This example requires the availability of a local SQL Server 2008 R2 Express instance (&ldquo;.\SQLEXPRESS&rdquo;). If you have another version of SQL Server installed on your computer, set <strong>data source</strong> in the connection string (the Web.config file) to the corresponding SQL Server instance name.</p>
+<p>All Getting Started Examples: <a href="http://www.devexpress.com/Support/Center/p/E3543.aspx"><u>Lesson 1</u></a> <a href="http://www.devexpress.com/Support/Center/p/E3547.aspx"><u>Lesson 2</u></a> <a href="http://www.devexpress.com/Support/Center/p/E3603.aspx"><u>Lesson 3</u></a> <a href="http://www.devexpress.com/Support/Center/p/E3548.aspx"><u>Lesson 4</u></a> <strong>Lesson 5</strong></p>
+
+<br/>
+
+
